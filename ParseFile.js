e7c96b9fee9bc389ab24429b3d7f9f91f3cf3b47
svgrossarth <40278194+svgrossarth@users.noreply.github.com>
2018-08-21 09:17:29 -0700

    Working on selecting different reports

--- ParseFile.js
+++ ParseFile.js
@@ -1,14 +1,81 @@
 
 var XLSX = require('xlsx');
 
+var fileInput = document.getElementById("inputFile");
+fileInput.addEventListener('change', FindFile);
+
+// var selector = document.getElementById("theSelector");
+// selector.addEventListener('change', CorrectReport);
+
+/*function CorrectReport(e) {
+    var fileInput = document.getElementById("inputFile");
 
 
-function FindFile(){
-    var filePath = document.getElementById("inputFile");
-    console.log(filePath.value);
-    var ar = filePath.value.split("\\");
-    var fileName = ar[ar.length - 1];
-    var workBook = XLSX.readFile(fileName);
+    if(e.target.value == "Total Count"){
+        fileInput.addEventListener('change', TotalCount);
+    }
+    else if(e.target.value == "Convert Aries Query"){
+        fileInput.addEventListener('change', AriesQuery);
+
+    }
+
+}*/
+
+function TotalCount (sheetAr){
+    for(let i = 0; i < sheetAr.length; i++){
+        for(let j = 0; j < sheetAr.length; j++){
+            if(sheetAr[i]["ID's"] == sheetAr[j]["ID's"]){
+                if(i != j){
+                    removeSubjectDuplicates(sheetAr, i, j);
+                    sheetAr[i]["Count"] = Number(sheetAr[i]["Count"]) + Number(sheetAr[j]["Count"]);
+                    sheetAr[i]["Count"] = sheetAr[i]["Count"].toString();
+                    sheetAr.splice(j, 1);
+                    console.log(sheetAr.length);
+                    j--;
+                }
+            }
+        }
+
+    }
+}
+
+function AriesQuery (sheetAr){
+    console.log("test");
+}
+
+
+function FindFile(e){
+    var file = e.target.files[0];
+
+    //this bit here is the magic that takes the file and makes it so it is read able by sheetjs
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var data = e.target.result;
+        data = new Uint8Array(data);
+        var workBook = XLSX.read(data, {type: 'array'});
+        var arSheets = workBook.SheetNames;
+        var workSheet = workBook.Sheets[arSheets[0]];
+        var sheetAr = XLSX.utils.sheet_to_json(workSheet);
+        var selector = document.getElementById("theSelector");
+        if(selector.value == "Total Count"){
+            TotalCount(sheetAr);
+        }
+        else if(selector.value == "Convert Aries Query"){
+            AriesQuery(sheetAr);
+
+        }
+        // ParseSheet(sheetAr);
+        // CreateNewExcel(sheetAr);
+    };
+    reader.readAsArrayBuffer(file);
+
+
+  /*  console.log(e.target.files[0].path);
+   // var ar = filePath.value.split("\\");
+   // var fileName = ar[ar.length - 1];
+    var workBook = XLSX.read(filePath);
+    //XLSX.writeFile(workBook, 'testing.xlsx');
+   // var workBook = XLSX.readFile(fileName);
     var arSheets = workBook.SheetNames;
     var workSheet = workBook.Sheets[arSheets[0]];
     var sheetAr = XLSX.utils.sheet_to_json(workSheet);
