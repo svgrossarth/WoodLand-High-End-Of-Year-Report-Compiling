4d2c44ef6d865fe252786257a4c490f7a8cdcbe1
svgrossarth <40278194+svgrossarth@users.noreply.github.com>
2018-08-30 12:29:36 -0700

    made the css file, not much in there. Working on changing how you select multiple files, from one input area to one input area per file

--- ParseFile.js
+++ ParseFile.js
@@ -1,9 +1,101 @@
 
 var XLSX = require('xlsx');
 
-var fileInput = document.getElementById("inputFile");
-fileInput.addEventListener('change', FindFile);
+var theSelector =  document.getElementById("theSelector");
+theSelector.addEventListener('change', UpdateDOMForFileSelection);
 
+/*
+var fileInput = document.getElementById("inputFile");
+fileInput.addEventListener('change', FindFileInital);
+*/
+
+
+function AttachInputTextInital(textNode){
+    var selectorDiv = document.getElementById("selectorDiv");
+    var input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("id", "fileInput1");
+    selectorDiv.appendChild(input);
+    var textDiv1 = document.createElement("Div");
+    textDiv1.setAttribute("id", "text1");
+    textDiv1.appendChild(textNode);
+    selectorDiv.appendChild(textDiv1);
+    var fileInput = document.getElementById("fileInput1");
+    fileInput.addEventListener('change', FindFileInital);
+}
+
+function AttachInputTextRec(textNodeArray, i, totalNumFiles) {
+    var inputDiv = document.getElementById("inputDiv");
+    var innerInputDiv = document.createElement("div");
+    innerInputDiv.setAttribute("id","innerInputDiv" + i);
+    innerInputDiv.style.display = "flexbox";
+    innerInputDiv.style.flexDirection = "row";
+    inputDiv.appendChild(innerInputDiv);
+    var innerFileInput = document.createElement("input");
+    innerFileInput.setAttribute("type", "file");
+    innerFileInput.setAttribute("id", "fileInput" + i + 1);
+    innerInputDiv.appendChild(innerFileInput);
+    var newTextDiv = document.createElement("Div");
+    newTextDiv.setAttribute("id", "text" + i + 1);
+    newTextDiv.appendChild(textNodeArray[i]);
+    innerInputDiv.appendChild(newTextDiv);
+    var fileInput = document.getElementById("fileInput" + i + 1);
+    fileInput.addEventListener('change', FindFileInital);
+
+    if(i == totalNumFiles - 1){
+
+    }else{
+    }
+
+
+
+}
+
+
+function UpdateDOMForFileSelection(e) {
+    var textNodeArray = [];
+    var textNode = document.createTextNode("Please Select Aries Query");
+    textNodeArray.push(textNode);
+    if(theSelector.value == "Total Count"){
+        var textNodeAttendance = document.createTextNode("Please Select Attendence File");
+        AttachInputTextInital(textNodeAttendance);
+    }else if(theSelector.value == "Convert Aries Query Fall"){
+        AttachInputTextInital(textNode);
+    }else if(theSelector.value == "Update ETS Roster"){
+        var textNode1 = document.createTextNode("Please Select ETS Roster");
+        textNodeArray.push(textNode1);
+        AttachInputTextInital(textNode);
+        AttachInputTextRec(textNodeArray, 1, 2);
+
+    }else if(theSelector.value == "Update Migrant Ed Roster"){
+        var textNode1 = document.createTextNode("Please Select Migrant Ed Roster");
+        textNodeArray.push(textNode1);
+        AttachInputTextInital(textNode);
+        AttachInputTextRec(textNodeArray, 1, 2);
+
+    }else if(theSelector.value == "Update PTS Roster"){
+        var textNode1 = document.createTextNode("Please Select PTS Roster");
+        textNodeArray.push(textNode1);
+        AttachInputTextInital(textNode);
+        AttachInputTextRec(textNodeArray, 1, 2);
+
+    }else if(theSelector.value == "Update ELD Roster"){
+        var textNode1 = document.createTextNode("Please Select ELD Roster");
+        textNodeArray.push(textNode1);
+        AttachInputTextInital(textNode);
+        AttachInputTextRec(textNodeArray, 1, 2);
+
+    }
+    else if(theSelector.value == "Convert Aries Query Fall With ALL Programs"){
+        var textNode1 = document.createTextNode("Please Select Excel Sheet Containing All Program Roster's");
+        textNodeArray.push(textNode1);
+        AttachInputTextInital(textNode);
+        AttachInputTextRec(textNodeArray, 1, 2);
+
+    }
+
+
+}
 
 function TotalCount (sheetAr){
     for(let i = 0; i < sheetAr.length; i++){
@@ -81,9 +173,107 @@ function AVIDChecker(outLoopStudent, innerLoopSubject) {
 }
 
 
-function FindFile(e){
-    var file = e.target.files[0];
+function TestFunc1(sheetAr) {
+    console.log("Single file test")
 
+}
+
+function HandleSheets(arraySheetAr) {
+    console.log("Test");
+
+}
+
+function ConvertFileToJSON(e, allFiles, i, arraySheetAr) {
+
+        var data = e.target.result;
+        data = new Uint8Array(data);
+        var workBook = XLSX.read(data, {type: 'array'});
+        var arSheets = workBook.SheetNames;
+        var workSheet = workBook.Sheets[arSheets[0]];
+        arraySheetAr.push(XLSX.utils.sheet_to_json(workSheet));
+        if(allFiles.length != arraySheetAr.length){
+            FindFile(allFiles, i, arraySheetAr);
+        }else{
+            HandleSheets(arraySheetAr);
+        }
+
+
+
+}
+
+function FindFileInital(eInitial){
+        var arraySheetAr = [];
+        var file = eInitial.target.files[0];
+        var allFiles = eInitial.target.files;
+        var reader = new FileReader();
+        reader.onload = function (e){
+            ConvertFileToJSON(e, allFiles, 0, arraySheetAr);
+
+        };
+        reader.readAsArrayBuffer(file);
+
+
+}
+
+function FindFile(allFiles, i, arraySheetAr){
+    i++;
+    //WORK HERE i NEED TO MAKE ONE OF THOSE INSTANT FUNCTION THINGS, SO THERE IS A CLOSURE
+   /* for (; i < allFiles.length; i++){
+        (function (file,allFiles, i, arraySheetAr) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                ConvertFileToJSON(e, allFiles, i, arraySheetAr);
+
+            }
+            reader.readAsArrayBuffer(file);
+        })(allFiles[i],allFiles, i, arraySheetAr);
+    }*/
+    var file = allFiles[i];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        ConvertFileToJSON(e, allFiles, i, arraySheetAr);
+
+    }
+    reader.readAsArrayBuffer(file);
+
+}
+
+
+/*
+function FindFile(e){
+
+    if(e.target.files.length == 1){
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var data = e.target.result;
+            data = new Uint8Array(data);
+            var workBook = XLSX.read(data, {type: 'array'});
+            var arSheets = workBook.SheetNames;
+            var workSheet = workBook.Sheets[arSheets[0]];
+            var sheetAr = XLSX.utils.sheet_to_json(workSheet);
+            TestFunc1(sheetAr);
+        };
+        reader.readAsArrayBuffer(file);
+
+    }
+    else if(e.target.files.length == 2){
+        var files = e.target.files;
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var data = e.target.result;
+            data = new Uint8Array(data);
+            var workBook = XLSX.read(data, {type: 'array'});
+            var arSheets = workBook.SheetNames;
+            var workSheet = workBook.Sheets[arSheets[0]];
+            var sheetAr = XLSX.utils.sheet_to_json(workSheet);
+            TestFunc1(sheetAr);
+        };
+        reader.readAsArrayBuffer(files);
+
+    }*/
+
+/*
     //this bit here is the magic that takes the file and makes it so it is read able by sheetjs
     var reader = new FileReader();
     reader.onload = function(e) {
@@ -93,7 +283,11 @@ function FindFile(e){
         var arSheets = workBook.SheetNames;
         var workSheet = workBook.Sheets[arSheets[0]];
         var sheetAr = XLSX.utils.sheet_to_json(workSheet);
-        var selector = document.getElementById("theSelector");
+        return sheetAr;
+    }
+    reader.readAsArrayBuffer(file);
+
+
         if(selector.value == "Total Count"){
             TotalCount(sheetAr);
         }
@@ -104,10 +298,11 @@ function FindFile(e){
         }
         // ParseSheet(sheetAr);
         // CreateNewExcel(sheetAr);
-    };
-    reader.readAsArrayBuffer(file);
 
-}
+
+
+
+}*/
 
 
 function ParseSheet(sheetAr){
