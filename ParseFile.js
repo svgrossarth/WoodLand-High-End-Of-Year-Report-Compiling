9dfd409e29d7caaa67d19778c7da468598c28417
svgrossarth <40278194+svgrossarth@users.noreply.github.com>
2018-09-04 09:30:03 -0700

    made the lay out better put some boxes to make more readable. Made it so I can pass in multiple files next is convert files to json

--- ParseFile.js
+++ ParseFile.js
@@ -3,6 +3,7 @@ var XLSX = require('xlsx');
 
 var theSelector =  document.getElementById("theSelector");
 theSelector.addEventListener('change', UpdateDOMForFileSelection);
+var arraySheetAr = [];
 
 /*
 var fileInput = document.getElementById("inputFile");
@@ -10,41 +11,63 @@ fileInput.addEventListener('change', FindFileInital);
 */
 
 
+function TheButGenerator(){
+    var theSubBut = document.createElement("button");
+    theSubBut.setAttribute("id", "submitButton");
+    theSubBut.setAttribute("type", "button");
+    var theButText = document.createTextNode("Submit");
+    theSubBut.appendChild(theButText);
+    theSubBut.addEventListener("click", FindFile);
+    return theSubBut;
+
+}
+
 function AttachInputTextInital(textNode){
-    var selectorDiv = document.getElementById("selectorDiv");
+    var innerInputDiv0 = document.getElementById("innerInputDiv0");
     var input = document.createElement("input");
     input.setAttribute("type", "file");
-    input.setAttribute("id", "fileInput1");
-    selectorDiv.appendChild(input);
+    input.setAttribute("id", "fileInput0");
+    input.style.borderStyle = "solid";
+    innerInputDiv0.appendChild(input);
     var textDiv1 = document.createElement("Div");
     textDiv1.setAttribute("id", "text1");
     textDiv1.appendChild(textNode);
-    selectorDiv.appendChild(textDiv1);
-    var fileInput = document.getElementById("fileInput1");
-    fileInput.addEventListener('change', FindFileInital);
+    textDiv1.style.paddingLeft = "5px";
+    textDiv1.style.paddingRight = "5px";
+    innerInputDiv0.appendChild(textDiv1);
+    var fileInput = document.getElementById("fileInput0");
+    //fileInput.addEventListener('change', FindFileInital);
 }
 
 function AttachInputTextRec(textNodeArray, i, totalNumFiles) {
-    var inputDiv = document.getElementById("inputDiv");
+    var alignerDiv = document.getElementById("aligner");
     var innerInputDiv = document.createElement("div");
     innerInputDiv.setAttribute("id","innerInputDiv" + i);
-    innerInputDiv.style.display = "flexbox";
+    innerInputDiv.style.display = "flex";
     innerInputDiv.style.flexDirection = "row";
-    inputDiv.appendChild(innerInputDiv);
+    alignerDiv.appendChild(innerInputDiv);
     var innerFileInput = document.createElement("input");
     innerFileInput.setAttribute("type", "file");
-    innerFileInput.setAttribute("id", "fileInput" + i + 1);
+    innerFileInput.setAttribute("id", "fileInput" + i);
+    innerFileInput.style.borderStyle = "solid";
     innerInputDiv.appendChild(innerFileInput);
     var newTextDiv = document.createElement("Div");
     newTextDiv.setAttribute("id", "text" + i + 1);
+    newTextDiv.style.paddingLeft = "5px";
+    newTextDiv.style.paddingRight = "5px";
     newTextDiv.appendChild(textNodeArray[i]);
     innerInputDiv.appendChild(newTextDiv);
     var fileInput = document.getElementById("fileInput" + i + 1);
-    fileInput.addEventListener('change', FindFileInital);
+   // fileInput.addEventListener('change', FindFileInital);
 
     if(i == totalNumFiles - 1){
+        var theSubBut = TheButGenerator();
+        innerInputDiv.appendChild(theSubBut);
+
 
     }else{
+        i++;
+        AttachInputTextRec(textNodeArray, i, totalNumFiles);
     }
 
 
@@ -59,8 +82,12 @@ function UpdateDOMForFileSelection(e) {
     if(theSelector.value == "Total Count"){
         var textNodeAttendance = document.createTextNode("Please Select Attendence File");
         AttachInputTextInital(textNodeAttendance);
+        var theSubBut = TheButGenerator();
+        document.getElementById("innerInputDiv0").appendChild(theSubBut);
     }else if(theSelector.value == "Convert Aries Query Fall"){
         AttachInputTextInital(textNode);
+        var theSubBut = TheButGenerator();
+        document.getElementById("innerInputDiv0").appendChild(theSubBut);
     }else if(theSelector.value == "Update ETS Roster"){
         var textNode1 = document.createTextNode("Please Select ETS Roster");
         textNodeArray.push(textNode1);
@@ -191,23 +218,13 @@ function ConvertFileToJSON(e, allFiles, i, arraySheetAr) {
         var arSheets = workBook.SheetNames;
         var workSheet = workBook.Sheets[arSheets[0]];
         arraySheetAr.push(XLSX.utils.sheet_to_json(workSheet));
-        if(allFiles.length != arraySheetAr.length){
-            FindFile(allFiles, i, arraySheetAr);
-        }else{
-            HandleSheets(arraySheetAr);
-        }
-
-
-
 }
 
-function FindFileInital(eInitial){
-        var arraySheetAr = [];
-        var file = eInitial.target.files[0];
-        var allFiles = eInitial.target.files;
+function FindFileInital(e){
+        var file = e.target.files[0];
         var reader = new FileReader();
         reader.onload = function (e){
-            ConvertFileToJSON(e, allFiles, 0, arraySheetAr);
+            ConvertFileToJSON(e);
 
         };
         reader.readAsArrayBuffer(file);
@@ -215,9 +232,7 @@ function FindFileInital(eInitial){
 
 }
 
-function FindFile(allFiles, i, arraySheetAr){
-    i++;
-    //WORK HERE i NEED TO MAKE ONE OF THOSE INSTANT FUNCTION THINGS, SO THERE IS A CLOSURE
+function FindFile(){
    /* for (; i < allFiles.length; i++){
         (function (file,allFiles, i, arraySheetAr) {
             var reader = new FileReader();
@@ -228,14 +243,18 @@ function FindFile(allFiles, i, arraySheetAr){
             reader.readAsArrayBuffer(file);
         })(allFiles[i],allFiles, i, arraySheetAr);
     }*/
-    var file = allFiles[i];
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        ConvertFileToJSON(e, allFiles, i, arraySheetAr);
+    var alignerDiv = document.getElementById("aligner");
+    var numOfFiles = alignerDiv.children.length;
+    var arOfInputs = alignerDiv.children;
+    for(var i = 0; i < numOfFiles; i++){(function (file){
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            ConvertFileToJSON(e, allFiles, i, arraySheetAr);
 
-    }
-    reader.readAsArrayBuffer(file);
+        }
+        reader.readAsArrayBuffer(file);
 
+    })(arOfInputs[i].children[0].files[0])}
 }
 
 
