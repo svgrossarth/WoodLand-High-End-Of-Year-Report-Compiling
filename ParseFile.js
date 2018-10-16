2c3375bce2655a5aae6053472e8a6cfd06ffd9eb
svgrossarth <40278194+svgrossarth@users.noreply.github.com>
2018-10-16 08:16:04 -0700

    Mostly implemented ASSETs report, still has some bugs

--- ParseFile.js
+++ ParseFile.js
@@ -5,6 +5,7 @@ const arrayOfPossibleChoices = ["Total Count", "Convert Aries Query Fall", "Conv
     "Update ETS Roster", "Update Migrant Ed Roster", "Update PTS Roster", "Update ELD Roster",
     "Monthly Lunch ASSETs Report", "Monthly After School ASSETs Report"];
 
+//Triggered when the user selects what they wish to generate
 var theSelector =  document.getElementById("theSelector");
 theSelector.addEventListener('change', UpdateDOMForFileSelection);
 
@@ -18,13 +19,19 @@ var objSheetAr = {
     eldRoster: "",
 };
 
-
+//when objSheetAr has any of it's values changed the handler triggers
 const theHandler = {
-    set(obj, prop, value){
-        if(prop == "periodAttendance" && value != ""){
+    set(obj, prop, value) {
+        if (prop == "periodAttendance" && theSelector.value === arrayOfPossibleChoices[0]) {
             let sheetAr = TotalCount(objSheetAr["periodAttendance"]);
             CreateNewExcel(sheetAr, "TotalCountReport.xlsx")
-        }else if(prop == "ariesQuery" && value !="" && obj["etsRoster"] == "" && obj["ptsRoster"] == "" && obj["migRoster"] == "" && obj["eldRoster"] == ""){
+        }else if (prop === "periodAttendance" && theSelector.value === arrayOfPossibleChoices[7]){
+            let sheetAr = ASSETsReport(objSheetAr["periodAttendance"]);
+            CreateNewExcel(sheetAr, "ASSETsLunchReport.xlsx", true);
+        }else if (prop === "periodAttendance" && theSelector.value === arrayOfPossibleChoices[8]){
+            let sheetAr = ASSETsReport(objSheetAr["periodAttendance"]);
+            CreateNewExcel(sheetAr, "ASSETsAfterSchoolReport.xlsx", true);
+        } else if(prop == "ariesQuery" && value !="" && obj["etsRoster"] == "" && obj["ptsRoster"] == "" && obj["migRoster"] == "" && obj["eldRoster"] == ""){
             var sheetAr = AriesQuery(objSheetAr["ariesQuery"]);
             CreateNewExcel(sheetAr, "Parsed Aries Query No Programs.xlsx");
         }else if(obj["ariesQuery"] != "" && obj["etsRoster"] != "" && obj["ptsRoster"] != "" && obj["migRoster"] != "" && obj["eldRoster"] != ""){
@@ -37,7 +44,186 @@ const theHandler = {
 
 var theProxy = new Proxy(objSheetAr, theHandler);
 
+function Student() {
 
+        this.Count = "";
+        this.StudentName = "";
+        this.Grade = "";
+        this.StudentID = "";
+        this.Lunch = "";
+        this.Subject = "";
+        this.empty_1 = "";
+        this.empty_2 = "";
+
+    for(let i = 1; i < 32; i++){
+        this[i] = "";
+    }
+}
+
+function Timez(timeString) {
+    let amPm = timeString.split(" ")[1];
+    let timeNums = timeString.split(" ")[0];
+    let hour = timeNums.split(":")[0];
+    let minutes = timeNums.split(":")[1];
+    let partOfHour = 0;
+    this.absoluteTime = 0;
+    if(amPm === "AM"){
+        partOfHour = minutes / 60;
+        this.absoluteTime = Number(hour) + partOfHour;
+    }else {
+        partOfHour = minutes / 60;
+        if(hour == 12){
+            this.absoluteTime = Number(hour) + partOfHour;
+        }else {
+            this.absoluteTime = 12 + Number(hour) + partOfHour;
+        }
+
+
+
+    }
+
+}
+
+
+function TimeInCenter(timeOut, timeIn) {
+    let time_In = new Timez(timeIn);
+    let time_out = new Timez(timeOut);
+    let exactTime = time_out.absoluteTime - time_In.absoluteTime;
+    let wholeTime = parseInt(exactTime, 10);
+    let justDecimal = exactTime - wholeTime;
+    if(justDecimal >= 0 && .25 > justDecimal){
+        justDecimal = 0;
+    }else if(justDecimal >= .25 && .5 > justDecimal){
+        justDecimal = .25;
+    }else if (justDecimal >= .5 && .75 > justDecimal){
+        justDecimal = .5;
+    }else if (justDecimal >= .75){
+        justDecimal = .75;
+    }
+    if((wholeTime < 0) || (wholeTime < 1)){
+        return 1;
+    }else {
+        return wholeTime + justDecimal;
+    }
+
+}
+
+function removePeriodFromClass(objectContainer) {
+    let justClass = "";
+    let longClass = "";
+    justClass = objectContainer["Subject"].split("-");
+    if(justClass.length > 2){
+        for(let i = 0; i < justClass.length - 1; i++){
+            if(i === 0) {
+                longClass += justClass[i + 1].trim();
+            }else{
+                longClass += " - " + justClass[i + 1];//note English 1 - 2 has spaces while Pre-Calculus doesn't,
+                                                        // here Pre-Calculus will now be Pre - Calculus
+            }
+        }
+        justClass = longClass;
+
+    }else if(justClass[1]){
+        justClass = justClass[1].trim() ;
+    }else{
+        justClass = justClass[0].trim();
+    }
+
+    objectContainer["Subject"] = justClass;
+
+}
+
+function BuildStudentOb (ASSETsAr, periodAttendanceRow, dayOfTheMonth, period, counter){
+    let alreadyBeenCounted = false;
+    let studentOb = new Student();
+    for(var i = 0; i < ASSETsAr.length; i++){
+        if(ASSETsAr[i].StudentID === periodAttendanceRow["Student ID"]){
+            alreadyBeenCounted = true;
+            break;
+        }
+    }
+
+    if(alreadyBeenCounted){
+        removePeriodFromClass(periodAttendanceRow);
+        removeSubjectDuplicates(ASSETsAr[i], periodAttendanceRow, "Subject");
+        if(periodAttendanceRow["Other Subject"]){
+            ASSETsAr[i]["Subject"] += ", " + periodAttendanceRow["Other Subject"];
+        }
+        //ASSETsAr[i].Subject += ", " + periodAttendanceRow["Subject"];
+        ASSETsAr[i][dayOfTheMonth] = TimeInCenter(periodAttendanceRow["Time Out"], periodAttendanceRow["Time In"]);
+
+    }else {
+
+        studentOb.Count = counter;
+        studentOb.Grade = periodAttendanceRow["Grade"];
+        studentOb.StudentID = periodAttendanceRow["Student ID"];
+        studentOb.StudentName = periodAttendanceRow["First Name"] + " " + periodAttendanceRow["Last Name"];
+        removePeriodFromClass(periodAttendanceRow);
+        studentOb.Subject = periodAttendanceRow["Subject"];
+        if(periodAttendanceRow["Other Subject"]){
+            studentOb.Subject += ", " + periodAttendanceRow["Other Subject"];
+        }
+
+        if(period === "Lunch"){
+            studentOb[dayOfTheMonth] = "1";
+            studentOb.Lunch = "Y";
+        }else{
+            studentOb.Lunch = "N";
+            studentOb[dayOfTheMonth] = TimeInCenter(periodAttendanceRow["Time Out"], periodAttendanceRow["Time In"]);
+
+        }
+
+    }
+
+    return studentOb;
+
+}
+
+
+function BuildASSETs(periodAttendanceRow, period, counter, ASSETsAr) {
+        let studentOb = new Student();
+        let theDate = periodAttendanceRow.Date;
+        let splitDate = theDate.split("/");
+        let dayOfTheMonth = splitDate[1];
+
+
+
+        if(period === "Lunch" && periodAttendanceRow["Period"] === "Lunch") {
+            studentOb = BuildStudentOb(ASSETsAr, periodAttendanceRow, dayOfTheMonth, "Lunch", counter);
+
+        }else if(period === "After School" && periodAttendanceRow["Period"] === "After School") {
+            studentOb = BuildStudentOb(ASSETsAr, periodAttendanceRow, dayOfTheMonth, "After School", counter);
+
+        }
+
+    return studentOb;
+
+}
+
+
+function ASSETsReport(periodAttendance) {
+    let ASSETsAr = [];
+    let count = 1;
+    for(let i = 0; i < periodAttendance.length; i++){
+        if(periodAttendance[i]["Student ID"] !== undefined) {
+            let theStudent;
+            if (theSelector.value === arrayOfPossibleChoices[7]) {
+                theStudent = BuildASSETs(periodAttendance[i], "Lunch", count, ASSETsAr);
+                if (theStudent.Count !== "") {
+                    count++;
+                    ASSETsAr.push(theStudent);
+                }
+            } else if (theSelector.value === arrayOfPossibleChoices[8]) {
+                theStudent = BuildASSETs(periodAttendance[i], "After School", count, ASSETsAr);
+                if (theStudent.Count !== "") {
+                    count++;
+                    ASSETsAr.push(theStudent);
+                }
+            }
+        }
+    }
+    return ASSETsAr;
+}
 
 
 function InsertPrograms(theStudent) {
@@ -90,6 +276,7 @@ function AddPrograms(sheetAr) {
 
 }
 
+//creates the Submit button
 function TheButGenerator(){
     var theSubBut = document.createElement("button");
     theSubBut.setAttribute("id", "submitButton");
@@ -100,7 +287,7 @@ function TheButGenerator(){
     return theSubBut;
 }
 
-
+//Run after the user hits submit
 function DetermineRequest() {
     if(theSelector.value == arrayOfPossibleChoices[0]){
         GetSingleFile("periodAttendance");
@@ -113,9 +300,12 @@ function DetermineRequest() {
         GetEachProgramRoster();
     }else if(theSelector.value === arrayOfPossibleChoices[7]){
         GetSingleFile("periodAttendance");
+    }else if(theSelector.value === arrayOfPossibleChoices[8]){
+        GetSingleFile("periodAttendance");
     }
 }
 
+//Pulls the different files and converts each
 function GetEachProgramRoster() {
     var alignerDiv = document.getElementById("aligner");
     var numOfFiles = alignerDiv.children.length;
@@ -187,7 +377,7 @@ function ConvertSheetToJSONAllPrograms(e){
 
 }
 
-
+//takes in the single file submitted by the user to then convert to a JSON
 function GetSingleFile(sheetName){
     var file = document.getElementById("fileInput0").files[0];
     var reader = new FileReader();
@@ -249,7 +439,7 @@ function AttachInputTextRec(textNodeArray, i, totalNumFiles) {
 
 }
 
-
+//Allows user to select which files they wish to use to generate the report
 function UpdateDOMForFileSelection(e) {
     var textNodeArray = [];
     var textNode = document.createTextNode("Please Select Aries Query");
@@ -297,10 +487,16 @@ function UpdateDOMForFileSelection(e) {
         var textNodeAttendance = document.createTextNode("Please Select Attendance File");
         textNodeArray.push(textNodeAttendance);
         AttachInputTextInital(textNodeAttendance);
-        AttachMonthInput();
+        //AttachMonthInput();
         var theSubBut = TheButGenerator();
         document.getElementById("innerInputDiv0").appendChild(theSubBut);
-
+    }else if(theSelector.value === arrayOfPossibleChoices[8]){
+        var textNodeAttendance = document.createTextNode("Please Select Attendance File");
+        textNodeArray.push(textNodeAttendance);
+        AttachInputTextInital(textNodeAttendance);
+        //AttachMonthInput();
+        var theSubBut = TheButGenerator();
+        document.getElementById("innerInputDiv0").appendChild(theSubBut);
     }
 }
 
@@ -339,24 +535,27 @@ function AddAndRemoveKeys(student) {
 
 }
 
-function ConcatenateOtherSubject(sheetAr, i, j){
-    if(sheetAr[i]["Other Subject"] === undefined && !(sheetAr[j]["Other Subject"] === undefined)) {
-        sheetAr[i]["Other Subject"] = sheetAr[j]["Other Subject"];
-    }else if(!(sheetAr[i]["Other Subject"] === undefined) && !(sheetAr[j]["Other Subject"] === undefined)){
-        sheetAr[i]["Other Subject"] =  sheetAr[i]["Other Subject"] + "," + sheetAr[j]["Other Subject"];
+function ConcatenateOtherSubject(oldList, newList){
+    if(oldList["Other Subject"] === undefined && !(newList["Other Subject"] === undefined)) {
+        oldList["Other Subject"] = newList["Other Subject"];
+    }else if(!(oldList["Other Subject"] === undefined) && !(newList["Other Subject"] === undefined)){
+        oldList["Other Subject"] =  oldList["Other Subject"] + ", " + newList["Other Subject"];
     }
 }
+
 
 function TotalCount (sheetAr){
     for(let i = 0; i < sheetAr.length; i++){
         if(!(sheetAr[i]["Student ID"] === undefined)) {
             AddAndRemoveKeys(sheetAr[i]);
+            removePeriodFromClass(sheetAr[i]);
             for (let j = 0; j < sheetAr.length; j++) {
                 if (!(sheetAr[j]["Student ID"] === undefined)) {
                     if (sheetAr[i]["Student ID"] === sheetAr[j]["Student ID"]) {
                         if (i !== j) {
-                            removeSubjectDuplicates(sheetAr, i, j);
-                            ConcatenateOtherSubject(sheetAr, i, j);
+                            removePeriodFromClass(sheetAr[j]);
+                            removeSubjectDuplicates(sheetAr[i], sheetAr[j], "Subject");
+                            ConcatenateOtherSubject(sheetAr[i], sheetAr[j]);
                             let innerCount = CheckForCount(sheetAr[j]);
                             if (innerCount) {
                                 sheetAr[i]["Count"] = Number(sheetAr[i]["Count"]) + Number(sheetAr[j]["Count"]);
@@ -461,7 +660,7 @@ function AVIDChecker(outLoopStudent, innerLoopSubject) {
 
 }
 
-
+//Converts excel file to JSON and updates array with converted sheets and the proxy
 function ConvertSheetToJSON(e, correctSheet, sheetname) {
 
         var data = e.target.result;
@@ -476,8 +675,18 @@ function ConvertSheetToJSON(e, correctSheet, sheetname) {
 }
 
 
-function CreateNewExcel(sheetAr, newExcelName){
-    var newSheet = XLSX.utils.json_to_sheet(sheetAr);
+
+
+function CreateNewExcel(sheetAr, newExcelName, specialSheet = false){
+    if(specialSheet){
+        var newSheet = XLSX.utils.json_to_sheet(sheetAr, {header:["Count","StudentName","Grade",
+                        "StudentID","Lunch","1","2","3","4","5","6","7","8","9","10","11","12","13",
+                        "14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31",
+                        "empty_1","empty_2","Subject"]});
+    }else {
+        var newSheet = XLSX.utils.json_to_sheet(sheetAr);
+    }
+
     var newWorkBook = XLSX.utils.book_new();
     var convertedSheet = "The compiled Sheet";
     XLSX.utils.book_append_sheet(newWorkBook, newSheet, convertedSheet);
@@ -492,7 +701,7 @@ function  UpdateUserAboutFile(newExcelname) {
     
 }
 
-function removeSubjectDuplicates(sheetAr, i, j){
+/*function removeSubjectDuplicates(sheetAr, i, j){
     if(!(sheetAr[i]["Subject"] === undefined) && !(sheetAr[j]["Subject"] === undefined)) {
         var outterLoopSubjects = sheetAr[i]["Subject"].split(",");
         var innerLoopSubjects = sheetAr[j]["Subject"].split(",");
@@ -514,4 +723,30 @@ function removeSubjectDuplicates(sheetAr, i, j){
             newUniqueSubject = true;
         }
     }
+}*/
+
+function removeSubjectDuplicates(orginalList, newList, keyword){
+    if(!(orginalList[keyword] === undefined) && !(newList[keyword] === undefined) && !(orginalList[keyword] === "") && !(newList[keyword] === "")) {
+        var outterLoopSubjects = orginalList[keyword].split(",");
+        var innerLoopSubjects = newList[keyword].split(",");
+        var newUniqueSubject = true;
+
+        for (let j2 = 0; j2 < innerLoopSubjects.length; j2++) {
+            for (let i2 = 0; i2 < outterLoopSubjects.length; i2++) {
+                if (innerLoopSubjects[j2].trim() === outterLoopSubjects[i2].trim()) {
+                    newUniqueSubject = false;
+                    break;
+                }
+            }
+
+            if (newUniqueSubject) {
+                orginalList[keyword] = orginalList[keyword] + ", " + innerLoopSubjects[j2].trim();
+                outterLoopSubjects = orginalList[keyword].split(",");
+            }
+
+            newUniqueSubject = true;
+        }
+    }
 }
+
+
